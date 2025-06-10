90e1c8635b6cf72b1a605cee07a915aa264f747f
ranggarap <fadhilahrangga26@gmail.com>
2025-06-10 10:36:34 +0700

    Implementasi awal struktur antrian menggunakan array

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+class Queues{
+    int FRONT, REAR, max =5;
+    int queues_array[5];
+public:
+    Queues() {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    
+};
