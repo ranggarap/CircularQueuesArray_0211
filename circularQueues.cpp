48d360e63ad9500adbbfbae7effff2fbb92cb396
ranggarap <fadhilahrangga26@gmail.com>
2025-06-10 10:44:24 +0700

    Menambahkan logika antrian melingkar untuk penempatan elemen

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -20,5 +20,18 @@ public:
         if ((FRONT == 0 && REAR == max -1) || (FRONT == REAR +1)){
             cout << "\nQueue overflow\n";
             return;
+        } 
+        // cek apakah antrian kosong
+        if (FRONT == -1){
+            FRONT =0;
+            FRONT=0;
         }
+        else{
+            // jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max -1)
+            REAR =0;
+            else 
+            REAR = REAR + 1;
+        }
+    }
 };
